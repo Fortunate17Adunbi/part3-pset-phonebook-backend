ba0527c015b3faa742393a5f66f1a915e8de8823
Adunbi Fortunate <samuelfortunate098@gmail.com>
2024-09-13 17:59:50 +0100

    Added config for update and delete

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/phonebook')
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:', request.path)
+    console.log('Body:', request.body)
+    console.log('---')
+    next()
+}
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(requestLogger)
 // app.use(morgan(':request'))
 app.use(morgan(':method :url - :status :res[content-length] - :response-time ms :request'))
 
@@ -42,14 +50,8 @@ let persons = [
 
 ]
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:', request.path)
-    console.log('Body:', request.body)
-    console.log('---')
-    next()
-}
-app.use(requestLogger)
+
+
 
 const generateId = () => {
     const maxId = persons.length > 0 ? 
@@ -71,14 +73,21 @@ app.get("/api/persons", (request, response) => {
     })
 })
 
-app.get("/info", (request, response) => {
-    const phonebookList = persons.length
-    const date = new Date()
-    response.send(`
-        <p>Phone book has info for ${phonebookList} people</p>
-        <br />
-        <p>${date}</p>
-    `)
+app.get("/info", (request, response, next) => {
+    // const phonebookList = persons.length
+    // const date = new Date()
+    // response.send(`
+    //     <p>Phone book has info for ${phonebookList} people</p>
+    //     <br />
+    //     <p>${date}</p>
+    // `)
+    Person.find({}).then(person => {
+        response.send(`
+            <p>Phone book has info for ${person.length} people</p>
+            <br />
+            <p>${new Date()}</p>
+        `)
+    }).catch(error => next(error))
 })
 
 app.get("/api/persons/:id", (request, response) => {
@@ -122,15 +131,40 @@ app.post("/api/persons/", (request, response) => {
 
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body
 
-   const person = persons.find(person => person.id === id)
-   persons = persons.filter(person => person.id !== id)
-   response.json(person)
-   response.status(204).end()
-   console.log(persons)
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(updatedNumber => {
+        response.json(updatedNumber)
+    }).catch(error => next(error))
 })
+
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(deletedNumber => {
+        response.json(deletedNumber)
+        response.status(204).end()
+    }).catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
